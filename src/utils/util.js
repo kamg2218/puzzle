1a83a27a086ee972a34eff47afc1cf53ee589987
윤현지 <yunhyeonji@yunhyeonjiui-MacBookAir.local>
2023-12-04 23:21:56 +0900

    move image utils

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,27 +1,3 @@
-import frenchBulldog from "../assets/image/french-bulldog.jpg";
-import retrieverDog from "../assets/image/retriever-dog.jpg";
-import shibaDog from "../assets/image/full-shot-shiba-dog.jpg";
-import frontViewShibaDog from "../assets/image/front-view-shiba-dog.jpg";
-
-export const IMAGE_POSITIONS = [
-  { x: "1%", y: "1%" },
-  { x: "50%", y: "1%" },
-  { x: "99%", y: "1%" },
-  { x: "1%", y: "50%" },
-  { x: "50%", y: "50%" },
-  { x: "99%", y: "50%" },
-  { x: "1%", y: "99%" },
-  { x: "50%", y: "99%" },
-  { x: "99%", y: "99%" },
-];
-
-export const IMAGE_LIST = [
-  frenchBulldog,
-  retrieverDog,
-  shibaDog,
-  frontViewShibaDog,
-];
-
 export const shuffle = (array) => {
   let idx = array.length - 1;
   while (idx > 0) {
